145cce2513e316ece66916d4a65897fa093a9741
johannes.wijaya <johannes.wijaya@idexpress.com>
2024-03-23 11:13:29 +0700

    another ui improvement (add placeholder)

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   const universalTime = useReadUniversalTime()
   
   return (
-    <div className="container justify-center items-center mx-auto h-screen flex flex-col">
+    <div className="container justify-center items-center mx-auto flex flex-col">
       <Control className="container h-14"></Control>
-      <div className="container mt-2 space-x-2 flex h-full">
+      <div className="container mt-2 space-x-2 flex h-full pb-10">
           <div className="grow h-14"></div>
           <div className="flex-none w-1/4 h-full">
             <Chef className="mt-2" play={universalTime.isTicking}></Chef>
